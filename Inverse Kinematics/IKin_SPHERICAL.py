dd9ee4cf443b02443682638ec64190ebfd3089ea
Raychatronics09 <157681561+Raychatronics09@users.noreply.github.com>
2024-02-29 21:25:26 +0800

    Update IKin_SPHERICAL.py

--- Inverse Kinematics/IKin_SPHERICAL.py
+++ Inverse Kinematics/IKin_SPHERICAL.py
@@ -1,41 +1,37 @@
-### inverse kinematics - SPHERICAL
+### Inverse Kinematics - SPHERICAL
 
 import numpy as np
 
 
-#link lengths
+# Link Lengths
 
 a1 = float(input("a1 = "))
 a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 
 
-#joint variable
+# Joint Variable
 
 x0_3 = float(input("x0_3 = "))
 y0_3 = float(input("y0_3 = "))
 z0_3 = float(input("z0_3 = "))
 
 
-## inverse knematics solution using graphical method
+## Inverse Kinematics using graphical method solutions
 
-T1 = np.arctan(y0_3 / x0_3)
+T1 = np.arctan(y0_3 / x0_3) * (180/np.pi)
 
 r1 = np.sqrt(x0_3**2 + y0_3**2)
 
 r2 = z0_3 - a1
 
-T2 = np.arctan(r2 / r1)
+T2 = np.arctan(r2 / r1) * (180/np.pi)
 
 d3 = np.sqrt(r1**2 + r2**2) - a2 - a3
 
 #-----------------------------------------------------#
 
-d1 =  z0_3- a2 - a3
-
-# displaying the joint variable
-
-
+# Displaying the Joint Variables T1, T2, d3
 
 print("T1 = " , np.around(T1,3))
 print("T2 = " , np.around(T2,3))
