277d40283ba2ed6b9c5e1aefa4ec90c3263477d3
Gian Carl Tolentino <157565160+jheeyan@users.noreply.github.com>
2024-04-05 19:36:12 +0800

    Update Model.py

--- Model (wip).py
+++ Model (wip).py
@@ -14,17 +14,11 @@ a1 = float(input("a1 = "))
 a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 
-def mm_to_m(a):
-    m = 1000 ##meter to mm
-    return a/m
 
-a1 = mm_to_m(a1)
-a2 = mm_to_m(a2)
-a3 = mm_to_m(a3)
 
 ##  PRISMATIC LINK LIMIT mm - m
 lml = float(input("lml = "))
-lml = mm_to_m(lml)
+
 
 ## CREATE LINKS
 ## Robot_variable = DHRobot([RevoluteDH(d,r,alpha,offset=theta,qlim)])
@@ -32,9 +26,9 @@ lml = mm_to_m(lml)
 
 
 Spherical = DHRobot([
-    RevoluteDH(a1,0,(90/180)*np.pi,(0/180)*np.pi,qlim=[-np.pi/2,np.pi/2]),
+    RevoluteDH(a1/100,0,(90/180)*np.pi,(0/180)*np.pi,qlim=[-np.pi/2,np.pi/2]),
     RevoluteDH(0,0,(90/180)*np.pi,(90/180)*np.pi,qlim=[-np.pi/2,np.pi/2]),
-    PrismaticDH(0,0,(0/180)*np.pi,a1+a2,qlim=[0,lml]),
+    PrismaticDH(0,0,(0/180)*np.pi,(a3+a2)/100,qlim=[0,lml/100]),
 ], name="Spherical") 
 
 print(Spherical)
